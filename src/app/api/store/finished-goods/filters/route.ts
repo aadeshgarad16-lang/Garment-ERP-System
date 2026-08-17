8f0ad25c69415a7cef6cd486f9d9f297633cb2d0
aadeshgarad16-lang <Aadeshgarad16@gmail.com>
2026-08-17 07:49:09 +0530

    chore: remove backend API routes and database libraries from git tracking

--- src/app/api/store/finished-goods/filters/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
-  return NextResponse.json({
-    categories: ['Shirts', 'Trousers', 'Jackets', 'Accessories'],
-    sizes: ['S', 'M', 'L', 'XL', 'XXL'],
-    colors: ['Red', 'Blue', 'Black', 'White', 'Green', 'Yellow', 'Pink', 'Purple', 'Orange', 'Grey']
-  });
-}
